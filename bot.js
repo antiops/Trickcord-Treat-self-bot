5656e465cfef74777df2d45d5819544d7f8e198f
Anti Ops <22041463+antiops@users.noreply.github.com>
2020-10-16 14:52:36 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,12 +4,13 @@ const client = new Discord.Client()
 
 const getDateString = () => {
     let date = new Date()
-    return `${date.getDate().toString().padStart(2, '0')}.` +
-      `${(date.getMonth() + 1).toString().padStart(2, '0')}.` +
+    return `${date.getDate().toString().padStart(2, '0')}-` +
+      `${(date.getMonth() + 1).toString().padStart(2, '0')}-` +
       `${date.getFullYear()} ` +
       `${date.getHours().toString().padStart(2, '0')}:` +
       `${date.getMinutes().toString().padStart(2, '0')}:` +
-      `${date.getSeconds().toString().padStart(2, '0')}`
+      `${date.getSeconds().toString().padStart(2, '0')}.` +
+      `${date.getMilliseconds().toString().padStart(2, '0')}`
 }
 
 client.on('ready', () => {
@@ -19,17 +20,19 @@ client.on('ready', () => {
 client.on('message', msg => {
   // ID of Trick'cord Treat#1944
   if (msg.author.id === '755580145078632508') {
-    const botMessage = msg.embeds[0].description
-    const [trick, treat] = ['h!trick','h!treat']
+    if (msg.guild.id === process.env.SERVER) {
+      const botMessage = msg.embeds[0].description
+      const [trick, treat] = ['h!trick','h!treat']
 
-    console.log(`[${getDateString()}][Info] New message in server: ${msg.guild.name} (${botMessage})`)
-    if (botMessage.includes(treat)) {
-      msg.channel.send(treat)
-      console.log(`[${getDateString()}][Info] Sent ${treat} in channel: #${msg.channel.name}`)
-    }
-    else if (botMessage.includes(trick)) {
-      msg.channel.send(trick)
-      console.log(`[${getDateString()}][Info] Sent ${trick} in channel: #${msg.channel.name}`)
+      console.log(`[${getDateString()}][Info] New message in server: ${msg.guild.name} (${botMessage})`)
+      if (botMessage.includes(treat)) {
+        msg.channel.send(treat)
+        console.log(`[${getDateString()}][Info] Sent ${treat} in channel: ${msg.channel.name}`)
+      }
+      else if (botMessage.includes(trick)) {
+        msg.channel.send(trick)
+        console.log(`[${getDateString()}][Info] Sent ${trick} in channel: #${msg.channel.name}`)
+      }
     }
   }
 })
